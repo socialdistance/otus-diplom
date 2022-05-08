335d317a74404eb0f7af0744fe2a70b2b98dfed1
socialdistance <pleaseuseurbrain@protonmail.com>
2022-05-08 23:10:29 +0300

    test4

--- internal/gather/memory/memory.go
+++ internal/gather/memory/memory.go
@@ -16,7 +16,7 @@ type loadStats struct {
 }
 
 func Get() (*Stats, error) {
-	ret, err := ux.SysctlRaw("vm.swapusage")
+	ret, err := ux.SysctlRaw("vm.swapusage") //nolint:typecheck
 	if err != nil {
 		return nil, fmt.Errorf("failed in sysctl vm.swapusage: %w", err)
 	}
